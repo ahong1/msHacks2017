9c447d504deee2b1b3962a359cf2674eed47467f
jesuisyeji <anyeji@gmail.com>
2017-03-25 17:45:42 -0700

    modified:   NutriGram/.idea/misc.xml 	modified:   NutriGram/app/src/main/AndroidManifest.xml 	new file:   NutriGram/app/src/main/java/Overlay.java 	new file:   NutriGram/app/src/main/java/OverlayFile

---
+++ NutriGram/app/src/main/java/Overlay.java
@@ -0,0 +1,51 @@
+/**
+ * Created by Yeji An on 3/25/2017.
+ */
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
+import
+
+public class Overlay {
+
+    public final static int Overlay_REQUEST_CODE = 251;
+
+    public void checkDrawOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(mActivity)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, Overlay_REQUEST_CODE);
+            } else {
+                openFloatingWindow();
+            }
+        } else {
+            openFloatingWindow();
+        }
+    }
+
+    private void openFloatingWindow() {
+        Intent intent = new Intent(mActivity, FloatingWindow.class);
+        mActivity.stopService(intent);
+        mActivity.startService(intent);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case Overlay_REQUEST_CODE: {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (Settings.canDrawOverlays(mActivity)) {
+                        openFloatingWindow();
+                    }
+                } else {
+                    openFloatingWindow();
+                }
+                break;
+            }
+        }
+    }
+}
